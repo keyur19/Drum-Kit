0842caf44eb7719395e0ccb45160a8ab53a39207
keyur19 <rachchh.khushi30@gmail.com>
2025-07-16 13:55:11 +0530

    change folder structure and move index.js file

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 
 
 document.querySelector(".w.drum").addEventListener("click", function() {
-    let audio = new Audio("sounds/tom-1.mp3");
+    let audio = new Audio("./Drum Kit Starting Files/sounds/tom-1.mp3");
     audio.play();
     buttonAnimation("w");
 });
 document.querySelector(".a.drum").addEventListener("click", function() {
-    let audio = new Audio("sounds/crash.mp3");
+    let audio = new Audio("./Drum Kit Starting Files/sounds/crash.mp3");
     audio.play();
     buttonAnimation("a");
 });
 document.querySelector(".s.drum").addEventListener("click", function() {
-    let audio = new Audio("sounds/tom-2.mp3");
+    let audio = new Audio("./Drum Kit Starting Files/sounds/tom-2.mp3");
     audio.play();
     buttonAnimation("s");
 });
 document.querySelector(".d.drum").addEventListener("click", function() {
-    let audio = new Audio("sounds/kick-bass.mp3");
+    let audio = new Audio("./Drum Kit Starting Files/sounds/kick-bass.mp3");
     audio.play();
     buttonAnimation("d");
 });
 document.querySelector(".j.drum").addEventListener("click", function() {
-    let audio = new Audio("sounds/tom-3.mp3");
+    let audio = new Audio("./Drum Kit Starting Files/sounds/tom-3.mp3");
     audio.play();
     buttonAnimation("j");
 });
 let k = document.querySelector(".k.drum").addEventListener("click", function() {
-    let audio = new Audio("sounds/tom-4.mp3");
+    let audio = new Audio("./Drum Kit Starting Files/sounds/tom-4.mp3");
     audio.play();
     buttonAnimation("k");
 });
 document.querySelector(".l.drum").addEventListener("click", function() {
-    let audio = new Audio("sounds/snare.mp3");
+    let audio = new Audio("./Drum Kit Starting Files/sounds/snare.mp3");
     audio.play();
     buttonAnimation("l");
 });
@@ -44,32 +44,32 @@ document.addEventListener("keydown", function(event){
 function makeSound(key){
     switch(key){
         case "w" : 
-        let audio = new Audio("sounds/tom-1.mp3");
+        let audio = new Audio("./Drum Kit Starting Files/sounds/tom-1.mp3");
         audio.play();
         break;
 
         case "a" :
-            let audio1 = new Audio("sounds/crash.mp3");
+            let audio1 = new Audio("./Drum Kit Starting Files/sounds/crash.mp3");
             audio1.play();
             break;
         case "s" :
-            let audio2 = new Audio("sounds/tom-2.mp3");
+            let audio2 = new Audio("./Drum Kit Starting Files/sounds/tom-2.mp3");
             audio2.play();
             break;
         case "d" :
-            let audio3 = new Audio("sounds/kick-bass.mp3");
+            let audio3 = new Audio("./Drum Kit Starting Files/sounds/kick-bass.mp3");
             audio3.play();
             break;
         case "j" :
-            let audio4 = new Audio("sounds/tom-3.mp3");
+            let audio4 = new Audio("./Drum Kit Starting Files/sounds/tom-3.mp3");
             audio4.play();
             break;
         case "k" :
-            let audio5 = new Audio("sounds/tom-4.mp3");
+            let audio5 = new Audio("./Drum Kit Starting Files/sounds/tom-4.mp3");
             audio5.play();
             break;
         case "l" :
-            let audio6 = new Audio("sounds/snare.mp3");
+            let audio6 = new Audio("./Drum Kit Starting Files/sounds/snare.mp3");
             audio6.play();
             break;
         default : alert("press appropriate key");
